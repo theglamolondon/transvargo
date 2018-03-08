96ed310e5b0b2d87ec6e4f83a6d6efa6a4fd051b
Darnels <akejeansidoine@yahoo.fr>
2018-03-08 23:12:00 +0000

    New Modifs

---
+++ app/Mail/NotifTransvargoTeam.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use App\Expedition;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NotifTransvargoTeam extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $expedition;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Expedition $expedition)
+    {
+        $this->expedition = $expedition;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from (env('MAIL_USERNAME'))
+            ->view('email.notif-team-transvargo', ["expedition" => $this->expedition]);
+    }
+}
