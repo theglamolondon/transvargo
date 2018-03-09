ba9e08f954e5d2415712b41f933c28399f87c306
Wilfried KOFFI <glamolondon@gmail.com>
2018-03-09 23:34:24 +0000

    Begin map choice

--- resources/views/staff/map/affectation.blade.php
+++ resources/views/staff/map/affectation.blade.php
@@ -14,33 +14,56 @@
             <form action="" method="post">
                 {{ csrf_field() }}
                 <div class="col-md-2 col-sm-12">
-                <div class="col-md-12">
                     <div class="col-md-12">
-                        <b>Expédition</b>
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="N° d'expédition" name="reference" id="reference" value="{{ request()->query("reference") }}">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i> </span>
+                        <div class="col-md-12">
+                            <b>Expédition</b>
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="N° d'expédition" disabled name="reference" id="reference" value="{{ $expedition->reference }}">
+                                <input type="hidden" name="expedition_id" value="{{ $expedition->id }}">
+                            </div>
                         </div>
-                        <br/><br/>
-                    </div>
 
-                    <div class="col-md-12">
-                        <b>Véhicule</b>
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Immatriculation" name="immatriculation" id="immatriculation" value="{{ request()->query("immatriculation") }}">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i> </span>
+                        <div class="col-md-12">
+                            <b>Transporteur</b>
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Nom du transporteur" disabled name="transporteur" id="transporteur" value="">
+                            </div>
                         </div>
-                        <br/><br/>
-                    </div>
 
-                    <div class="col-md-12">
-                        <div class="input-group">
-                            <button id="search" type="button" class="btn btn-sm bg-dark">Rechercher</button>
+                        <div class="col-md-12">
+                            <b>Véhicule</b>
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Immatriculation" disabled name="immatriculation" id="immatriculation" value="">
+                                <input type="hidden" name="vehicule_id" id="vehicule_id">
+                            </div>
                         </div>
-                        <br/><br/>
+
+                        <div class="col-md-12">
+                            <b>Prix de l'expédition</b>
+                            <div class="input-group">
+                                <input type="number" class="form-control" placeholder="prix de l'expediton" name="prix" id="prix" value="0">
+                            </div>
+                        </div>
+
+                        @if($expedition->isassure)
+                            <div class="col-md-12">
+                                <b>Prix de l'assurance</b><br>
+                                <span>{{ $expedition->assurance->libelle }}</span>
+                                <div class="input-group">
+                                    <input type="number" class="form-control" placeholder="prix de l'assurance" name="mttassurance" id="mttassurance" value="0">
+                                </div>
+                            </div>
+                        @endif
+
+                        <div class="col-md-12">
+                            <br><br><br>
+                            <div class="input-group">
+                                <button id="search" type="submit" class="btn btn-sm bg-dark">Choisir ce transporteur</button>
+                            </div>
+                            <br/><br/>
+                        </div>
                     </div>
                 </div>
-            </div>
             </form>
 
             <div class="col-md-10 col-sm-12">
@@ -52,7 +75,10 @@
 
 @section('script')
     <script type="application/javascript">
-        let EXPEDITIONS_ACTIVES = {!! json_encode() !!};
+        let VEHICULES = {!! json_encode($localisations, JSON_UNESCAPED_UNICODE) !!};
+        let VEHICULE_ACTIF = {};
+        let URL = "#_#_";
+        let URL_ITINERAIRE = '{{route("staff.expeditions.itineraire", ['immatriculation' => "_#1_", 'refrerence' => $expedition->reference ])}}';
 
         function initMap(){
             let map = new google.maps.Map(document.getElementById('map'), {
@@ -64,53 +90,47 @@
 
             let infoWindow = new google.maps.InfoWindow;
 
-            $.get("{{ route('staff.expeditions.localisation') }}", function (data) {
-                console.log(data);
-                let URL = '{{route("staff.offre.details", ['refrerence' => "_#_"])}}';
-                let URL_ITINERAIRE = '{{route("staff.expeditions.itineraire", ['immatriculation' => "_#1_", 'refrerence' => "_#2_"])}}';
-
-                for(i = 0; i < data.length; i++)
+            for(i = 0; i < VEHICULES.length; i++)
+            {
+                if( VEHICULES[i].coord != null)
                 {
-                    if( data[i].position !== null)
-                    {
-                        let marker = new google.maps.Marker({
-                            map: map,
-                            animation: google.maps.Animation.DROP,
-                            position: new google.maps.LatLng(parseFloat(data[i].position.latitude), parseFloat(data[i].position.longitude)),
-                            icon: '{{ asset('working/truck-map-marker30x42.png') }}',
-                            tag : data[i],
-                        });
+                    $coords = VEHICULES[i].coord.split(',');
+                    let marker = new google.maps.Marker({
+                        map: map,
+                        animation: google.maps.Animation.DROP,
+                        position: new google.maps.LatLng(parseFloat($coords[0]), parseFloat($coords[1])),
+                        icon: '{{ asset('working/truck-map-marker30x42.png') }}',
+                        tag : VEHICULES[i],
+                    });
 
-                        let infowincontent = "<div>"
-                            +"<a href='"+URL.replace("_#_",data[i].expedition.reference)+"'>"
-                            +"<strong>Expédition " + data[i].expedition.reference+"</strong></a><br/>"
-                            +"<b>Transporteur</b> : " + data[i].expedition.chargement.vehicule.transporteur.raisonsociale + " (" +data[i].expedition.chargement.vehicule.transporteur.nom +" "+ data[i].expedition.chargement.vehicule.transporteur.prenoms +")<br/>"
-                            +"<b>Véhicule</b> : " + data[i].expedition.chargement.vehicule.immatriculation + " (" +data[i].expedition.type_camion.libelle + ")<br/>"
-                            +"<b>Chauffeur</b> : " + data[i].expedition.chargement.vehicule.chauffeur + " (<i class='glyphicon glyphicon-phone'></i>"+ data[i].expedition.chargement.vehicule.telephone +")" +"<br/>"
-                            +"<hr style='display: block; width: 100%; margin: 4px 0;'/>"
-                            +"<a href='"+ (URL_ITINERAIRE.replace("_#1_",data[i].expedition.chargement.vehicule.immatriculation)).replace("_#2_",data[i].expedition.reference)+"'>Afficher l'itinéraire ...</a>"+"<br/>"
-                            +"</div>";
+                    let infowincontent = "<div>"
+                        +"<a href='"+URL.replace("_#_",VEHICULES[i].immatriculation)+"'>"
+                        +"<b>Transporteur</b> : " + VEHICULES[i].raisonsociale + " (" +VEHICULES[i].nom +" "+ VEHICULES[i].prenoms +")</a><br/>"
+                        +"<b>Téléphone</b> : " + VEHICULES[i].contact  + "<br/>"
+                        +"<b>Véhicule</b> : " + VEHICULES[i].immatriculation + " (" +VEHICULES[i].typecamion + ")<br/>"
+                        +"<b>Chauffeur</b> : " + VEHICULES[i].chauffeur + " (<i class='glyphicon glyphicon-phone'></i>"+ VEHICULES[i].ch_telephone +")" +"<br/>"
+                        +"<hr style='display: block; width: 100%; margin: 4px 0;'/>"
+                        +"<a href='"+ (URL_ITINERAIRE.replace("_#1_",VEHICULES[i].immatriculation))+"'>Afficher l'itinéraire ...</a>"+"<br/>"
+                        +"</div>";
 
-                        marker.addListener('click', function() {
-                            toggleBounce(marker);
-                            //marker.setAnimation(google.maps.Animation.BOUNCE);
-                            infoWindow.setContent(infowincontent);
-                            infoWindow.open(map, marker);
-                        });
+                    marker.addListener('click', function() {
+                        toggleBounce(marker);
+                        infoWindow.setContent(infowincontent);
+                        infoWindow.open(map, marker);
+                    });
 
-                        EXPEDITIONS_ACTIVES[data[i].expedition.reference] = marker;
-                    }
+                    VEHICULE_ACTIF[VEHICULES[i].immatriculation] = marker;
                 }
+            }
 
-                function toggleBounce(marker)
-                {
-                    if (marker.getAnimation() !== null) {
-                        marker.setAnimation(null);
-                    } else {
-                        marker.setAnimation(google.maps.Animation.BOUNCE);
-                    }
+            function toggleBounce(marker)
+            {
+                if (marker.getAnimation() !== null) {
+                    marker.setAnimation(null);
+                } else {
+                    marker.setAnimation(google.maps.Animation.BOUNCE);
                 }
-            });
+            }
         }
     </script>
     <script async defer
